893a111a5a0664a538fbe0aced0cb74db03cb947
ManikVaish88 <mannvaish@gmail.com>
2016-05-22 14:30:24 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#Steps to create a tiday dataset for only mean and standard deviation measures form all the variables.
+#Steps to create a tidy dataset for only mean and standard deviation measures form all the variables.
 #1. Extracts only the measurements on the mean and standard deviation for each measurement.
 #2. Appropriately labels the data set with descriptive variable names.
 #3. Merges the training and the test sets to create one data set.
